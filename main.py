031e38e7d4edc3cfe2956ab140224275bc379f25
Tien Loi Nguyen <tienloinguyen22@gmail.com>
2025-09-24 21:14:31 +0700

    Init model client

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-def main():
-    print("Hello from deep-research-agents!")
+import os
+import asyncio
+from dotenv import load_dotenv
+from autogen_ext.models.azure import AzureAIChatCompletionClient
+from azure.core.credentials import AzureKeyCredential
+from autogen_core.models import ModelInfo, ModelFamily, UserMessage
+
+async def main():
+  load_dotenv()
+  print("Hello from deep-research-agents!")
+
+  # init model & agents
+  model_client = AzureAIChatCompletionClient(
+    model="openai/gpt-4o",
+    endpoint="https://models.github.ai/inference",
+    credential=AzureKeyCredential(os.environ["PERSONAL_GITHUB_TOKEN"]),
+    model_info=ModelInfo(
+      vision=False,
+      function_calling=True,
+      json_output=True,
+      family=ModelFamily.GPT_4,
+      structured_output=True,
+    )
+  )
+
+  # test model
+  content = "What is the capital of Viet Nam?"
+  print("Question: ", content)
+
+  result = await model_client.create([UserMessage(content=content, source="user")])
+  print("Answer: ", result.content)
+  print("Cached: ", result.cached)
+  print("Finish Reason: ", result.finish_reason)
+  print("Thought: ", result.thought)
+  print("Usage: ", result.usage)
+
+
+  # close
+  await model_client.close()
 
 
 if __name__ == "__main__":
-    main()
+	asyncio.run(main())
